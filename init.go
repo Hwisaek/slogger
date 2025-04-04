b8bc040fe57b92434c0ea28407cc24c8eb8b5a1b
Hwisaek <hwisaek@gmail.com>
2025-04-04 23:38:54 +0900

    addSource 에 대한 옵션 추가

--- init.go
+++ init.go
@@ -5,8 +5,10 @@ import (
 	"os"
 )
 
-// Init
-// 컴파일 시 -trimpath 옵션을 추가하는 것을 권장
+// Init initializes the logger with the provided options.
+// If opt is nil, default options will be used.
+// It sets up a JSON logger with appropriate time formatting and level.
+// 컴파일 시 -trimpath 옵션을 추가하는 것을 권장합니다.
 func Init(opt *Option) error {
 	if opt != nil {
 		option = *opt
@@ -14,7 +16,8 @@ func Init(opt *Option) error {
 
 	var h contextHandler
 	h.Handler = slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{
-		Level: option.logLevel,
+		Level:     option.logLevel,
+		AddSource: option.addSource, // 원본 소스 위치를 추가하는 옵션 적용
 		ReplaceAttr: func(groups []string, a slog.Attr) slog.Attr {
 			switch a.Key {
 			case slog.TimeKey:
